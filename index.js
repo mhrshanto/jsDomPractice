6168b54f14ab5a383560fb8bda9b2f2917f16a56
Md Habibur Rahman Shanto <mhrshanto36@gmail.com>
2023-05-28 23:58:25 +0600

    URL seen code updated

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-let container = document.getElementsByClassName('container')[0];
+let container = document.getElementsByClassName('container')[0]
 
-let body = document.getElementsByTagName('body')[0];
+let heading = document.createElement('h2');
+
+heading.innerText = "Do you want to know our website URL?";
 
 
-let header = document.createElement("h1");
-document.removeChild('h2')[0];
 
-header.innerText = "Hello Programmers";
 
-body.appendChild(header);
+let button = document.createElement("button");
+let button2 = document.createElement("button");
+let text1 = "Yes"
+let text2 = "No"
+
+button.innerText = text1;
+button2.innerText = text2;
+
+function1 = () => {
+    let url = document.URL;
+
+    let text = document.createElement('p');
+    text.innerText = url;
+    container.appendChild(text)
+}
+
+button.addEventListener('click', function1)
+
+
+
+
+
+// Append 
+container.appendChild(heading)
+container.appendChild(button)
+container.appendChild(button2)
