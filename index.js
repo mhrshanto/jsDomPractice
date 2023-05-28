4513c2a673f4383175ae83e79c468bdeb7e0f0d5
Md Habibur Rahman Shanto <mhrshanto36@gmail.com>
2023-05-28 18:21:10 +0600

    Some code updated

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ btn.addEventListener('toggole', () => {
     btn.innerText = "Thank you";
 });
 
+let button = document.getElementById('btn');
+
+button.setAttribute("href", "www.codesplanner.com");
+button.setAttribute("class", "hello_btn");
+
+
+
+
+let heading = document.getElementById('heading');
+let subheading = document.getElementById('sub-heading');
+heading.addEventListener('click', () => {
+    heading.innerText = "How are you?"
+    subheading.remove();
+})
 
 
 
